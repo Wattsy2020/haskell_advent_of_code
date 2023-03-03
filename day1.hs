631fd834c9e434ff6f12ca8e417801490f943113
Liam Watts <liam.watts271@gmail.com>
2023-03-03 17:04:25 +0800

    day1: finish part1

--- day1.hs
+++ day1.hs
@@ -1,4 +1,5 @@
 import System.IO
+import Text.Parsec (parse)
 
 -- note this can be replaced by the `lines` function
 _splitLines :: [Char] -> String -> [String] -- split a string into new lines
@@ -7,13 +8,22 @@ _splitLines content prev_line
     | head content == '\n'  = prev_line : _splitLines (tail content) "" -- add prev_line to the list of lines, recurse with empty prev_line
     | otherwise             = _splitLines (tail content) (prev_line ++ [head content]) --recurse while adding to prev_line
 
+splitLines :: [Char] -> [String]
 splitLines str = _splitLines str ""
 
---TODO: use the empty "" to represent linebreaks (i.e. new elves), convert everything to integer
-parseLines :: [String] -> [[Int]]
-parseLines str = [[1]]
+_parseLines :: [String] -> [Int] -> [[Int]]
+_parseLines [] nums = [nums]
+_parseLines (str:strings) nums
+    | str == ""     = nums : _parseLines strings [] -- empty line denotes the start of a new elf, so we have fully parsed the previous elf
+    | otherwise     = _parseLines strings ((read str :: Int) : nums)
 
+parseLines :: [String] -> [[Int]]
+parseLines strings = _parseLines strings []
+
+-- calculate the elf with the most calories
+part1 :: String -> Int
+part1 contents = maximum (map sum ((parseLines . splitLines) contents))
 
 main = do
     contents <- readFile "problem_data/day1.txt"
-    print (splitLines contents)
+    print (part1 contents)
