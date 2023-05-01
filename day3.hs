5250e1f2ef4c1f6cd4a827c7b906eff9a61ded7d
Liam Watts <liam.watts271@gmail.com>
2023-05-02 01:18:28 +0800

    day3.hs: refactor common logic to process item groups

--- day3.hs
+++ day3.hs
@@ -2,20 +2,24 @@ import Data.Set qualified as Set
 import System.IO
 import Text.Parsec (parse)
 
--- store a rucksack as set of the first half and set of the second half
-type RuckSack = (Set.Set Char, Set.Set Char)
+-- a group of groups of items (either the halves of a rucksack, or the items of each of the three elves)
+type ItemGroup = [Set.Set Char]
+
+readItemGroup :: [String] -> ItemGroup
+readItemGroup = map Set.fromList
 
 -- part 1
-splitRucksack :: String -> RuckSack
-splitRucksack line = (Set.fromList $ take halfLength line, Set.fromList $ drop halfLength line)
+splitRucksack :: String -> ItemGroup
+splitRucksack line = readItemGroup [first, second]
   where
     halfLength = length line `div` 2
+    (first, second) = splitAt halfLength line
 
-readRucksacks :: String -> [RuckSack]
+readRucksacks :: String -> [ItemGroup]
 readRucksacks contents = map splitRucksack (lines contents)
 
-calcOverlap :: RuckSack -> Char
-calcOverlap (a, b) = Set.findMin $ Set.intersection a b -- findMin is a hack to get the single element in the set
+commonElem :: [Set.Set Char] -> Char
+commonElem = Set.findMin . foldl1 Set.intersection -- findMin is a hack to get the single element in the set
 
 -- very inefficient implementation, should learn to use Data.HashMap later
 priority :: Char -> Int
@@ -24,28 +28,18 @@ priority x = 1 + length (takeWhile (x /=) alphabet)
     alphabet = ['a' .. 'z'] ++ ['A' .. 'Z']
 
 -- calculate the sum of the priorities of the duplicate items
-part1 :: [RuckSack] -> Int
-part1 = sum . map (priority . calcOverlap)
+sumPriority :: [ItemGroup] -> Int
+sumPriority = sum . map (priority . commonElem)
 
-type ElfGroup = (Set.Set Char, Set.Set Char, Set.Set Char)
-
-readElfGroup :: String -> String -> String -> ElfGroup
-readElfGroup a b c = (Set.fromList a, Set.fromList b, Set.fromList c)
-
-_readElves :: [String] -> [ElfGroup]
+_readElves :: [String] -> [ItemGroup]
 _readElves [] = []
-_readElves (a : b : c : xs) = readElfGroup a b c : _readElves xs
+_readElves xs = readItemGroup elfGroup : _readElves elves
+  where
+    (elfGroup, elves) = splitAt 3 xs
 
-readElves :: String -> [ElfGroup]
+readElves :: String -> [ItemGroup]
 readElves = _readElves . lines
-
--- calculate the badge item that all three elves share
-badge :: ElfGroup -> Char
-badge (a, b, c) = Set.findMin $ foldl1 Set.intersection [a, b, c]
-
-part2 :: [ElfGroup] -> Int
-part2 = sum . map (priority . badge)
 
 main = do
   contents <- readFile "problem_data/day3.txt"
-  print (part1 (readRucksacks contents), part2 (readElves contents))
+  print (sumPriority (readRucksacks contents), sumPriority (readElves contents))
