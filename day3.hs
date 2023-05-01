017451e3db3a8ce48ffb55996ff45b772a0f5060
Liam Watts <liam.watts271@gmail.com>
2023-05-02 00:43:24 +0800

    day3: finish part2

--- day3.hs
+++ day3.hs
@@ -15,7 +15,7 @@ readRucksacks :: String -> [RuckSack]
 readRucksacks contents = map splitRucksack (lines contents)
 
 calcOverlap :: RuckSack -> Char
-calcOverlap (a, b) = Set.findMin (Set.intersection a b)
+calcOverlap (a, b) = Set.findMin $ Set.intersection a b -- findMin is a hack to get the single element in the set
 
 -- very inefficient implementation, should learn to use Data.HashMap later
 priority :: Char -> Int
@@ -27,6 +27,25 @@ priority x = 1 + length (takeWhile (x /=) alphabet)
 part1 :: [RuckSack] -> Int
 part1 = sum . map (priority . calcOverlap)
 
+type ElfGroup = (Set.Set Char, Set.Set Char, Set.Set Char)
+
+readElfGroup :: String -> String -> String -> ElfGroup
+readElfGroup a b c = (Set.fromList a, Set.fromList b, Set.fromList c)
+
+_readElves :: [String] -> [ElfGroup]
+_readElves [] = []
+_readElves (a : b : c : xs) = readElfGroup a b c : _readElves xs
+
+readElves :: String -> [ElfGroup]
+readElves = _readElves . lines
+
+-- calculate the badge item that all three elves share
+badge :: ElfGroup -> Char
+badge (a, b, c) = Set.findMin $ foldl1 Set.intersection [a, b, c]
+
+part2 :: [ElfGroup] -> Int
+part2 = sum . map (priority . badge)
+
 main = do
   contents <- readFile "problem_data/day3.txt"
-  print $ part1 (readRucksacks contents)
+  print (part1 (readRucksacks contents), part2 (readElves contents))
