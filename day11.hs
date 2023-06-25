a9be27b392b59926404af3ddc618d61c2906cf1b
Liam Watts <liam.watts271@gmail.com>
2023-06-26 00:58:03 +0800

    day11: implement monkey turns

--- day11.hs
+++ day11.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE RecordWildCards #-}
+
 import Data.List (stripPrefix)
 import Data.Maybe (fromJust)
+import Data.Sequence (Seq)
+import Data.Sequence qualified as Seq
 import Utils (split, strToInt)
 
 data Monkey = Monkey {items :: [Int], operation :: Int -> Int, test :: Int -> Bool, trueTarget :: Int, falseTarget :: Int}
@@ -44,9 +48,32 @@ parseMonkey monkeyStr =
   let [_, items, operation, test, trueTarget, falseTarget] = lines monkeyStr
    in Monkey (parseItems items) (parseOperation operation) (parseTest test) (parseTarget trueTarget) (parseTarget falseTarget)
 
-parseContents :: String -> [Monkey]
-parseContents = map parseMonkey . split "\n\n"
+parseContents :: String -> Seq Monkey
+parseContents = Seq.fromList . map parseMonkey . split "\n\n"
+
+addItem :: Int -> Monkey -> Monkey
+addItem item Monkey {..} = Monkey (items ++ [item]) operation test trueTarget falseTarget
+
+clearItems :: Monkey -> Monkey
+clearItems Monkey {..} = Monkey [] operation test trueTarget falseTarget
+
+monkeyInspect :: Monkey -> Seq Monkey -> Int -> Seq Monkey
+monkeyInspect monkey monkeys item = Seq.adjust (addItem worryLevel) monkeyTarget monkeys
+  where
+    worryLevel = div (operation monkey item) 3
+    monkeyTarget = if test monkey worryLevel then trueTarget monkey else falseTarget monkey
+
+monkeyTurn :: Seq Monkey -> Int -> Seq Monkey
+monkeyTurn monkeys idx = result
+  where
+    monkey = Seq.index monkeys idx
+    addedItems = foldl (monkeyInspect monkey) monkeys (items monkey)
+    result = Seq.adjust clearItems idx addedItems
+
+monkeyRound :: Seq Monkey -> Seq Monkey
+monkeyRound monkeys = foldl monkeyTurn monkeys [0 .. length monkeys - 1]
 
 main = do
   contents <- readFile "problem_data/day11.txt"
-  print $ parseContents contents
+  let monkeys = parseContents contents
+  print $ iterate monkeyRound monkeys !! 20
